656fd73202b5d4668f111d1376d5bcb7f1078b6d
gardusig <gustavo.gardusi@gmail.com>
2023-01-03 20:31:26 -0300

    Improved examples main

--- src/Examples/Main.cs
+++ src/Examples/Main.cs
@@ -6,22 +6,23 @@ using System.Threading;
 
 ConductorWorkflow workflow = Examples.Workflow.WorkflowCreator.CreateAndRegisterWorkflow();
 Examples.Worker.WorkerUtil.StartWorkers();
+StartWorkflowsAsync(workflow);
 
-WorkflowResourceApi workflowClient = Examples.Api.ApiUtil.GetClient<WorkflowResourceApi>();
-StartWorkflowRequest startWorkflowRequest = Examples.Workflow.WorkflowCreator.GetStartWorkflowRequest(workflow);
-string workflowId = workflowClient.StartWorkflow(startWorkflowRequest);
-
-Console.WriteLine();
-Console.WriteLine("=======================================================================================");
-Console.WriteLine("Workflow Execution Completed");
-Console.WriteLine($"Workflow Id: {workflowId}");
-
-Thread.Sleep(4 * 1000);
-Workflow receivedWorkflow = workflowClient.GetExecutionStatus(workflowId);
-
-Console.WriteLine($"Workflow Status: {receivedWorkflow.Status.ToString()}");
-Console.WriteLine($"Workflow Output: {receivedWorkflow.Output}");
-Console.WriteLine(
-    $"Workflow Execution Flow UI: {Examples.Api.ApiUtil.GetWorkflowExecutionURL(workflowId)}"
-);
-Console.WriteLine("=======================================================================================");
+static void StartWorkflowsAsync(ConductorWorkflow workflow)
+{
+    WorkflowResourceApi workflowClient = Examples.Api.ApiUtil.GetClient<WorkflowResourceApi>();
+    StartWorkflowRequest startWorkflowRequest = Examples.Workflow.WorkflowCreator.GetStartWorkflowRequest(workflow);
+    string workflowId = workflowClient.StartWorkflow(startWorkflowRequest);
+    Thread.Sleep(5 * 1000);
+    Console.WriteLine();
+    Console.WriteLine("=======================================================================================");
+    Console.WriteLine("Workflow Execution Completed");
+    Console.WriteLine("Workflow Id: " + workflowId);
+    Console.WriteLine("Workflow Execution Flow UI: " + Examples.Api.ApiUtil.GetWorkflowExecutionURL(workflowId));
+    Console.WriteLine("=======================================================================================");
+    Workflow receivedWorkflow = workflowClient.GetExecutionStatus(workflowId);
+    if (receivedWorkflow.Status != Workflow.StatusEnum.COMPLETED)
+    {
+        throw new Exception($"workflow not completed, workflowId: {workflowId}");
+    }
+}
