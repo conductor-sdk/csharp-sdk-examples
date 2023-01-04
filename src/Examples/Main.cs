32fbd061c2e2b95e84e0e53397ad816db9904b38
gardusig <gustavo.gardusi@gmail.com>
2023-01-04 10:17:30 -0300

    Example cleanup

--- src/Examples/Main.cs
+++ src/Examples/Main.cs
@@ -4,22 +4,26 @@ using Conductor.Definition;
 using System;
 using System.Threading;
 
-ConductorWorkflow workflow = Examples.Workflow.WorkflowCreator.CreateAndRegisterWorkflow();
 Examples.Worker.WorkerUtil.StartWorkers();
-StartWorkflowsAsync(workflow);
+StartWorkflowAsync();
 
-static void StartWorkflowsAsync(ConductorWorkflow workflow)
+static void StartWorkflowAsync()
 {
-    WorkflowResourceApi workflowClient = Examples.Api.ApiUtil.GetClient<WorkflowResourceApi>();
+    ConductorWorkflow workflow = Examples.Workflow.WorkflowCreator.CreateAndRegisterWorkflow();
     StartWorkflowRequest startWorkflowRequest = Examples.Workflow.WorkflowCreator.GetStartWorkflowRequest(workflow);
+
+    WorkflowResourceApi workflowClient = Examples.Api.ApiUtil.GetClient<WorkflowResourceApi>();
     string workflowId = workflowClient.StartWorkflow(startWorkflowRequest);
-    Thread.Sleep(5 * 1000);
+
+    Thread.Sleep(6 * 1000);
+
     Console.WriteLine();
     Console.WriteLine("=======================================================================================");
     Console.WriteLine("Workflow Execution Completed");
     Console.WriteLine("Workflow Id: " + workflowId);
     Console.WriteLine("Workflow Execution Flow UI: " + Examples.Api.ApiUtil.GetWorkflowExecutionURL(workflowId));
     Console.WriteLine("=======================================================================================");
+
     Workflow receivedWorkflow = workflowClient.GetExecutionStatus(workflowId);
     if (receivedWorkflow.Status != Workflow.StatusEnum.COMPLETED)
     {
