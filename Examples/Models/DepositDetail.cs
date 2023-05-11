881a66ec6df091acda96bf09e40dae152413fdc9
gardusig <gustavo.gardusi@gmail.com>
2023-05-11 06:21:00 -0300

    Added models

---
+++ Examples/Models/DepositDetail.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Examples.Models
+{
+    public class DepositDetail
+    {
+        public string accountId { get; set; }
+        public Decimal amount { get; set; }
+    }
+}
